ebd81963dc3b431ab61e35668ce6571e5f52b510
unknown <manishtomar998@gmail.com>
2022-02-28 17:13:03 +0530

    refresh

---
+++ Javascript/toDoList.js
@@ -0,0 +1,50 @@
+// let a = 10;
+// let b = 20;
+// let addTask = (a, b) => {
+//     var z = a + b;
+// }
+// addTask(a,b);
+// console.log(z)
+// let hello=()=>{
+//     console.log("fdgdfs");
+// }
+// setInterval(hello,2000);
+
+// let square = (z) => {
+//     var x = z * z;
+//     return x
+// }
+// addTask(a, b);
+
+// let say = (n, m, k) => {
+
+//     let arr = [calc(n), calc(m), calc(k)];
+//     console.log(arr)
+// }
+
+// let calc = (x) => {
+//     let result = x * 20 + 45;
+//     return result;
+// }
+
+// say(10, 40, 60);
+
+// let add = (a,b) => {
+//     // let result = 0;
+//     // for (let i = 0; i < b.length; i++) {
+//     //     result = result + b[i]
+//     // }
+//     console.log(a,b);
+// }
+
+// add(2, 5,6)
+// let arr = ["manish", "mayank", "robin", "savita", "shubham", "abhishant"];
+// let result = arr.filter(data => {
+//     let final = data.split("").sort();
+//     if(final[0]=="a"){
+//         if(final[0]==final[1]){
+//             return true;
+//         }
+//     }
+// })
+// console.log(result);
