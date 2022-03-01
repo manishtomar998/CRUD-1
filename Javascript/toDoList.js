9b040ddc29c213dbb91b3aa28270332d75520e8c
unknown <manishtomar998@gmail.com>
2022-03-01 17:16:27 +0530

    crud done

--- Javascript/toDoList.js
+++ Javascript/toDoList.js
@@ -48,3 +48,69 @@
 //     }
 // })
 // console.log(result);
+
+let addTask = () => {
+    let task = {
+        task: document.getElementById("task").value
+    };
+    let data = {
+        method: 'post',
+        body: JSON.stringify(task),
+        headers: {
+            "content-type": "application/json"
+        }
+    }
+    fetch("http://localhost:3000/tasks", data)
+}
+let showData = () => {
+    fetch("http://localhost:3000/tasks").then(response => {
+        response.text().then(res => {
+            createRow(JSON.parse(res));
+        })
+    })
+}
+let createRow = (res) => {
+    for (let index = 0; index < res.length; index++) {
+        let tableRow = document.createElement("tr");
+        let data1 = document.createElement("td");
+        let data2 = document.createElement("td");
+        let updateBtn = document.createElement("button");
+        updateBtn.onclick = () => {
+            updateTask(res[index]);
+        }
+        let deleteBtn = document.createElement("button");
+        deleteBtn.onclick = () => {
+            deleteTask(res[index]);
+        }
+        updateBtn.innerText = "Update"
+        deleteBtn.innerText = "Delete"
+        data1.innerText = res[index].id;
+        data2.innerText = res[index].task;
+        tableRow.appendChild(data1);
+        tableRow.appendChild(data2);
+        tableRow.appendChild(updateBtn);
+        tableRow.appendChild(deleteBtn);
+        document.getElementById("allData").appendChild(tableRow);
+    }
+}
+
+let updateTask = (item) => {
+    let task = { task: document.getElementById("task").value }
+    let data = {
+        body: JSON.stringify(task),
+        headers: {
+            "content-type": "application/json"
+        },
+        method: "put"
+    }
+    fetch(`http://localhost:3000/tasks/${item.id}`, data)
+}
+let deleteTask = (item) => {
+    let data = {
+        headers: {
+            "content-type": "application/json"
+        },
+        method: "delete"
+    }
+    fetch(`http://localhost:3000/tasks/${item.id}`, data)
+}
